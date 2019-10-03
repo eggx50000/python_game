945cf5db75893494b2ad3e7ba52818a5fe192086
joon <eggx50000@gmail.com>
2019-10-03 11:10:58 +0900

    이인용 네트워크

--- number_baseball.py
+++ number_baseball.py
@@ -1,4 +1,31 @@
-u=input('enter number: ')  
+u=input('enter number: ')
+def calculate(j,k):
+    if j==k:
+        return 'you win'
+        
+        
+    else:
+        ball=0
+        strike=0
+        for i in range(0,3):
+            if j[i]==k[i]:
+                strike+=1
+                    
+        for i in range(0,9):
+            if str(i) in j and str(i) in k:
+                ball+=1
+        ball-=strike
+        
+        if strike>=1 and ball>0:
+            return f'strike:{strike}, ball:{ball}!!!'
+                
+        elif strike>=1:
+            return f'strike:{strike}!!!'
+        elif ball>0:
+            return f'ball:{ball}!!!'
+        else:
+            return f'out!!!'
+# print(calculate('2648','2648'))
 from socket import *
 serverSock=socket(AF_INET,SOCK_STREAM)
 serverSock.bind(('', 8080))
@@ -7,60 +34,52 @@ serverSock.listen(1)
 clientSock, addr=serverSock.accept()
 
     
-    
-    
-
-
 
     
     
 print('<start!>\n')
 
 
-ball=0
-strike=0
+
+num=clientSock.recv(1024)
+num=num.decode(('utf-8'))
 n=1
 while True:
-    strike=0
-    ball=0
+    
     st=f'{n} round, 숫자를 입력: '
     clientSock.send(st.encode('utf-8'))
     msg=clientSock.recv(1024)
-    
-    
-    t=msg.decode('utf-8')
+    t=msg.decode(('utf-8'))
     if len(t)<=2:
         clientSock.send('다시하세요!!!'.encode('utf-8'))
         continue
-    if u==t:
-        clientSock.send('cleared'.encode('utf-8'))
+    result=calculate(u,t)
+        
+    
+              
+    clientSock.send(result.encode('utf-8'))
+    if 'you win'in result:
+        print('you lose')
         break
-    else:
-        for i in range(0,3):
-            if u[i]==t[i]:
-                strike+=1
-                
-        for i in range(0,9):
-            if str(i) in u and str(i) in t:
-                ball+=1
-        ball-=strike
-        result=''
-        if strike>=1 and ball>0:
-            result=f'strike:{strike}, ball:{ball}!!!'
-            
-        elif strike>=1:
-            result=f'strike:{strike}!!!'
-        elif ball>0:
-            result=f'ball:{ball}!!!'
-        else:
-            result=f'out!!!'
-        if n>=9:
-            result += ' game over'
-            
-        clientSock.send(result.encode('utf-8'))
-        if 'game over' in result:
-            break
-        n+=1
+    
+    g=input(st)
+    
+    if len(g)<=2:
+        print('다시하세요!!!')
+        continue
+    result=calculate(num,g)
+        
+    
+              
+    print(result)
+    if 'you win'in result:
+        print('you win')
+        clientSock.send('you lose'.encode('utf-8'))
+        break
+    n+=1
+    
+    
+        
 print('<the_end!>')
         
             
